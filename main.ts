124f9d10e14686e73f76511bfadefa8102ceff0b
WyLu112 <158294231+WyLu112@users.noreply.github.com>
2024-01-31 14:27:48 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    a = a + 1
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    b = b + 1
+})
+let b = 0
+let a = 0
+a = 0
+b = 0
